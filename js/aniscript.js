52621bbad13a67fbb3d52d07831bf84ade224e42
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-10-18 15:43:09 +0200

    rettet fonte, slidelink, moseover, m.m.

--- js/aniscript.js
+++ js/aniscript.js
@@ -1,37 +1,37 @@
-let button = document.querySelector("#boxContainer .one");
+let button = document.querySelector("#boxContainer .knap-one");
 
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
 
-    console.log("start er kørt");
+	console.log("start er kørt");
 
-    button.addEventListener("click", slide1);
+	button.addEventListener("click", slide1);
 
-    function slide1() {
-        console.log("slide1 er kørt");
-        document.querySelector(".one").classList.add("slideanimation");
+	function slide1() {
+		console.log("slide1 er kørt");
+		document.querySelector(".one").classList.add("slideanimation");
 
-        document.querySelector(".button2").addEventListener("click", slide2);
-    }
+		document.querySelector(".knap-two").addEventListener("click", slide2);
+	}
 
-    function slide2() {
-        console.log("slide2 er kørt");
-        document.querySelector(".two").classList.add("slideanimation");
+	function slide2() {
+		console.log("slide2 er kørt");
+		document.querySelector(".two").classList.add("slideanimation");
 
-        document.querySelector(".button3").addEventListener("click", slide3);
-    }
+		document.querySelector(".knap-three").addEventListener("click", slide3);
+	}
 
-    function slide3() {
-        console.log("slide3 er kørt");
-        document.querySelector(".three").classList.add("slideanimation");
+	function slide3() {
+		console.log("slide3 er kørt");
+		document.querySelector(".three").classList.add("slideanimation");
 
-        document.querySelector(".button4").addEventListener("click", slide4);
-    }
+		document.querySelector(".knap-four").addEventListener("click", slide4);
+	}
 
-    function slide4() {
-        console.log("slide4 er kørt");
-        document.querySelector(".four").classList.add("slideanimation");
-    }
+	function slide4() {
+		console.log("slide4 er kørt");
+		document.querySelector(".four").classList.add("slideanimation");
+	}
 
 }
